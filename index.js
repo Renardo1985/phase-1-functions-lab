a4dea698d526f073ba896eebd4e97ed678ce7d0a
Renardo Williams <rennwill@gmail.com>
2023-03-14 12:25:16 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location){
+// let location = "50th Street"
+//console.log(parseInt(`${location.slice(0,2)}`))
+//if (location > 42){return location - 42}
+//else if (location <= 42){return 42 - location}
+return (Math.abs(location - 42));
+}
+
+function distanceFromHqInFeet(location){
+    return (distanceFromHqInBlocks(location)*264);
+}
+
+function distanceTravelledInFeet(num1,num2){
+    return (Math.abs(num1-num2)*264)
+}
+
+function calculatesFarePrice(start,destination){
+
+    const distance = distanceTravelledInFeet(start,destination)
+    if (distance<400){return 0;}
+    else if (distance<2000) {return ((distance-400)*.02);}
+    else if (distance<2500) {return 25;}
+    else  return "cannot travel that far";
+}
+
+calculatesFarePrice(50,58);
